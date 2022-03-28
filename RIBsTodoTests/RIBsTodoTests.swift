a19407aba02099098ad348b808514e015fc7a68d
Allie Kim <allie0147@gmail.com>
2022-03-28 15:32:34 +0900

    Initial Commit

---
+++ RIBsTodoTests/RIBsTodoTests.swift
@@ -0,0 +1,33 @@
+//
+//  RIBsTodoTests.swift
+//  RIBsTodoTests
+//
+//  Created by Allie Kim on 2022/03/28.
+//
+
+import XCTest
+@testable import RIBsTodo
+
+class RIBsTodoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
