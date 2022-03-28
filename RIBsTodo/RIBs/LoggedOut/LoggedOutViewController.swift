c389ae035e7bc3e9efff32c7ff11df0efbe1dfa4
Allie Kim <allie0147@gmail.com>
2022-03-28 18:52:41 +0900

    Feat: Create `LoggedOutRIB`

---
+++ RIBsTodo/RIBs/LoggedOut/LoggedOutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoggedOutViewController.swift
+//  RIBsTodo
+//
+//  Created by Allie Kim on 2022/03/28.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol LoggedOutPresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class LoggedOutViewController: UIViewController, LoggedOutPresentable, LoggedOutViewControllable {
+
+    weak var listener: LoggedOutPresentableListener?
+}
